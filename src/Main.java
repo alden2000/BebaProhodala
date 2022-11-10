5a8adc861222349ad0be365e0fb94fb9e1cceb1f
Alden <aldenefendic0@gmail.com>
2022-11-11 00:31:52 +0100

    Heart Pattern 1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,7 @@
-class HeartPatternExample1
-{
-    // main() method start
-    public static void main(String[] args)
-    {
-        // declare and initialize variable for output size
-        final int size = 4  ;
+class HeartPatternExample1 {
+    public static void main(String[] args) {
+        int size = 4;
 
-        // nested for loop to print the upper part of Heart
         for (int m = 0; m < size; m++) {
             for (int n = 0; n <= 4 * size; n++) {
                 double pos1 = Math.sqrt(Math.pow(m - size, 2) + Math.pow(n - size, 2));
@@ -20,9 +15,7 @@ class HeartPatternExample1
             }
             System.out.print(System.lineSeparator());
         }
-       // for loop to print the lower part of Heart
-        for (int m = 1; m <= 2 * size; m++)
-        {
+        for (int m = 1; m <= 2 * size; m++) {
             for (int n = 0; n < m; n++) {
                 System.out.print(' ');
             }
