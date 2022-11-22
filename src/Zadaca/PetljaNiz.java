0a243404e923379a829c8794fee89e0c12985a67
Alden <aldenefendic0@gmail.com>
2022-11-22 19:24:40 +0100

    Zadaca

--- src/Zadaca/PetljaNiz.java
+++ src/Zadaca/PetljaNiz.java
@@ -1,23 +1,31 @@
 package Zadaca;
 
-//import java.util.Arrays;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PetljaNiz {
     public static void main(String[] args) {
-        int[] nizPogresnih=new int[100];
-        int brojac=0;
-        do {
-            System.out.println("Unesi peticu i zapocni igricu.." );
-            nizPogresnih[brojac] = new Scanner(System.in).nextInt();
-            brojac++;
-            if(nizPogresnih[brojac-1]!=5) {
-                for (int i = 0; i < brojac - 1; i++) {
-                    System.out.print(nizPogresnih[i] + " ");
+        int[] nizPogresnih=new int[1];
+        int broj;
+        int n=0;
+        while (true){
+            System.out.println("Unesi broj: ");
+            broj = new Scanner(System.in).nextInt();
+            if(broj==5){
+                System.out.println("Poceo si igricu.");
+                break;
+            }else {
+                nizPogresnih[n]=broj;
+                nizPogresnih = Arrays.copyOfRange(nizPogresnih, 0,nizPogresnih.length+1);
+//                nizPogresnih[n]=broj;
+                n++;
+                //nizPogresnih = Arrays.copyOfRange(nizPogresnih, 0,nizPogresnih.length);
+                String poruka = "Unio si ";
+                for (int i=0; i<nizPogresnih.length-1; i++) {
+                    poruka= poruka + nizPogresnih[i] + " ";
                 }
+                System.out.println(poruka+ "brojeve");
             }
-            System.out.println("");
-        }while (nizPogresnih[brojac-1]!=5);
-        System.out.println("super cista petica");
+        }
     }
 }
