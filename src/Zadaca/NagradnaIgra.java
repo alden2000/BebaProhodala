c487936663d6b7e15f754589e45e02b5aee0627e
Alden <aldenefendic0@gmail.com>
2022-11-23 16:39:52 +0100

    Zadaca2 nagradna igra

--- src/Zadaca/NagradnaIgra.java
+++ src/Zadaca/NagradnaIgra.java
@@ -1,21 +1,27 @@
 package Zadaca;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class NagradnaIgra {
     public static void main(String[] args) {
         int[] dobitniBrojevi = {13, 88, 49, 56, 27, 66, 33, 91};
-        int[] mojiBrojevi = new int[8];
+        //int[] mojiBrojevi = new int[1];
+        int broj;
         double nagrada = 10000.0, osvojenaNagrada = 10000.0;
         int brojUlaganja = 0;
 
         TESTIRANJE:
         for (; ; ) {
             brojUlaganja++;
+            int[] mojiBrojevi = new int[1];
             //int brojPokusaja = 0;
             System.out.println("Unesite " + (brojUlaganja) + ". pokusaj:");
-            for (int i = 0; i < mojiBrojevi.length; i++) {
-                mojiBrojevi[i] = new Scanner(System.in).nextInt();
+//            broj = new Scanner(System.in).nextInt();
+            for (int i = 0; i < 8; i++) {
+                broj = new Scanner(System.in).nextInt();
+                mojiBrojevi[i] = broj;
+                mojiBrojevi = Arrays.copyOfRange(mojiBrojevi, 0, mojiBrojevi.length + 1);
                 for (int j : dobitniBrojevi) {
                     if (mojiBrojevi[i] == j) {
                         System.out.println("Pogodili ste i osvojili nagradu od: ");
