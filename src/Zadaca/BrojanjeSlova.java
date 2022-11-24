b00f907e36360893d46b8554cf314713a058b0fb
ElvisHodzic98 <eldina.h_07.06@hotmail.com>
2022-11-24 19:24:43 +0100

    ispravka

--- src/Zadaca/BrojanjeSlova.java
+++ src/Zadaca/BrojanjeSlova.java
@@ -12,17 +12,29 @@ public class BrojanjeSlova {
         for (int i = 0; i < trazenaRijec.length; i++) {
             trazenaRijec[i] = new Scanner(System.in).nextLine().charAt(0);
         }
-        for (int i = 0; i < razlozenaRecenica.length; i++) {
+        LABELA: for (int i = 0; i < razlozenaRecenica.length; i++) {
+            if(postoji){
+                break ;
+            }
             if (razlozenaRecenica[i] != trazenaRijec[0]) {
                 continue;
             }
-            if (razlozenaRecenica[i + 1] == trazenaRijec[1] && razlozenaRecenica[i + 2] == trazenaRijec[2]) {
-                System.out.println("Vasa rijec pocinje od " + (i + 1) + ". znaka.");
-                postoji=true;
+            for (int j = 1; j < trazenaRijec.length; j++) {
+                if(razlozenaRecenica[i+j]==trazenaRijec[j]){
+                    //System.out.println("Vasa rijec pocinje od " + (i + 1) + ". znaka.");
+                    postoji = true;
+                }else {
+                    postoji = false;
+                    continue LABELA;
+                }
+//                System.out.println("Vasa rijec pocinje od " + (i + 1) + ". znaka.");
+//                postoji = true;
             }
         }
-        if(!postoji){
+        if (!postoji) {
             System.out.println("Ta rijec nije u recenici");
+        }else{
+            System.out.println("Ta rijec se nalazi u recenici");
         }
     }
 }
